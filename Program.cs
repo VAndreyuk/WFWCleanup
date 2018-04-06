23d6a8b050e5f9668ca1809546465e83843a60d6
VAndreyuk <vladimir.andreyuk@gmail.com>
2018-04-06 08:54:33 -0500

    initial commit;

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using NetFwTypeLib;
+using System;
+using System.Linq;
+
+namespace WFWCleanup
+{
+    /// <summary>
+    /// WFW cleanup helper.
+    /// </summary>
+    public class Program
+    {
+        private static bool _FixAll = false;
+
+        /// <summary>
+        /// Cleanups ALL Public inbound rules.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            _FixAll |= args?.Length > 0 && args[0] != null && args[0].ToLower().EndsWith("all");
+
+            Func<INetFwRule3, bool> filter = r => (_FixAll || r.Enabled)
+                && r.Action == NET_FW_ACTION_.NET_FW_ACTION_ALLOW
+                && r.Direction == NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN
+                && (r.Profiles & (int)NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC) > 0;
+
+            try
+            {
+                {
+                    var firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                    var rules = firewallPolicy.Rules.Cast<INetFwRule3>().ToList();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    var rulesToFix = rules.Where(filter).ToList();
+
+                    if (rulesToFix.Count == 0)
+                    {
+                        Console.WriteLine("No active public rules found.");
+                        WaitAndExit(false);
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    foreach (var rule in rulesToFix)
+                    {
+                        try
+                        {
+                            if (rule.Profiles == (int)NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC)
+                            {
+                                Console.WriteLine("Removing public profile rule: " + rule.Name);
+                                firewallPolicy.Rules.Remove(rule.Name);
+                                continue;
+                            }
+
+                            Console.WriteLine("Removing public profile from rule: " + rule.Name);
+                            rule.Profiles = rule.Profiles == (int)NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_ALL
+                                ? (int)(NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_DOMAIN | NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE)
+                                : rule.Profiles ^ (int)NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PUBLIC;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+
+                GC.Collect(0, GCCollectionMode.Forced, true);
+
+                if (!_FixAll)
+                {
+                    var firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                    var notFixedRules = firewallPolicy.Rules.Cast<INetFwRule3>().Where(filter).ToList();
+
+                    if (notFixedRules.Count == 0)
+                        WaitAndExit(true);
+
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    foreach (var backDoorRule in notFixedRules)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Disabling rule: " + backDoorRule.Name);
+                            backDoorRule.Enabled = false;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+
+                    GC.Collect(0, GCCollectionMode.Forced, true);
+                }
+
+                {
+                    var firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                    var notFixedRules = firewallPolicy.Rules.Cast<INetFwRule3>().Where(filter).ToList();
+
+                    if (notFixedRules.Count == 0)
+                        WaitAndExit(true);
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    foreach (var backDoorRule in notFixedRules)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Changing profile to private for rule: " + backDoorRule.Name);
+                            backDoorRule.Profiles = (int)NET_FW_PROFILE_TYPE2_.NET_FW_PROFILE2_PRIVATE;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                    }
+                }
+
+                GC.Collect(0, GCCollectionMode.Forced, true);
+
+                {
+                    var firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+                    var notFixedRules = firewallPolicy.Rules.Cast<INetFwRule3>().Where(filter).ToList();
+
+                    if (notFixedRules.Count == 0)
+                        WaitAndExit(true);
+
+                    foreach (var backDoorRule in notFixedRules)
+                    {
+                        Console.WriteLine("New back door rule found: " + backDoorRule.Name);
+                        Console.WriteLine("LocalAppPackageId: " + backDoorRule.LocalAppPackageId);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            WaitAndExit(false);
+        }
+
+        private static void WaitAndExit(bool @fixed)
+        {
+            if (@fixed)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("All rules was fixed!");
+            }
+
+            Console.Write("\r\nPres any key for exit: ");
+            Console.ReadKey();
+
+            Environment.Exit(0);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# WFWCleanup
+# WFWCleanup - Cleanup ALL Windows 10 firewall rules for Public profile
+
+##Purpose of this tool
+
+Some new Windows 10 firewall rules like:
+
+    @{Microsoft.PPIProjection_10.0.15063.0_neutral_neutral_cw5n1h2txyewy?ms-resource://Microsoft.PPIProjection/resources/ProductName}
+
+are not seen in the standart UI and it`s not easy to remove or disable them.
+
+In some cases you need to disconnect Wi-Fi first.
